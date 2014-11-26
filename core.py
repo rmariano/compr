df5eefc10cb8ae2eac3f44039d67ab6d1d2b69b1
Mariano Anaya <marianoanaya@gmail.com>
2014-11-25 22:46:59 -0300

    Start persisting contents.

--- core.py
+++ core.py
@@ -1,5 +1,8 @@
 import heapq
+import struct
 from collections import Counter
+
+ENC = 'utf-8'
 
 
 class CharNode(object):
@@ -72,3 +75,51 @@ def process_frequencies(stream):
     counts = Counter(stream)
     return [CharNode(value=value, freq=freq) for value, freq in counts.items()]
 
+
+def save_table(dest_file, table):
+    """Store the table in the destination file.
+    c: char
+    L: code of c (unsigned Long)"""
+    content = None  # of the file
+    offset = len(table)
+    tokens = [(bytes(char, encoding=ENC), v) for char, v in table.items()]
+    content = struct.pack('i', offset)
+    content += struct.pack('{}c'.format(offset), *[t[0] for t in tokens])
+    content += struct.pack('{}L'.format(offset), *[int(t[1], base=2) for t in tokens])
+    dest_file.write(content)
+
+
+def _sizeof(code):
+    sizes = {'i': 4, 'c': 1, 'L': 4}
+    return sizes.get(code, 1)
+
+
+def retrieve_table(dest_file):
+    """Read the binary file, and return the translation table as a reversed
+    dict."""
+    offset, *_ = struct.unpack('i', dest_file.read(_sizeof('i')))
+    chars = dest_file.read(offset * _sizeof('c'))
+    codes = dest_file.read(offset * _sizeof('L'))
+    chars = struct.unpack('{}c'.format(offset), chars)
+    codes = struct.unpack('{}L'.format(offset), codes)
+    return {bin(code): str(char, encoding=ENC) for char, code in zip(chars, codes)}
+
+
+def _brand_filename(filename):
+    return "{}.comp".format(filename)
+
+
+def save_compressed_file(filename, table):
+    """Given the original file by its <filename>, save a new one.
+    <table> contains the new codes for each character on <filename>"""
+    new_file = _brand_filename(filename)
+    with open(new_file, 'wb') as f:
+        save_table(f, table)
+
+
+def retrieve_compressed_file(filename):
+    """Reconstruct the original file from the compressed copy."""
+    fname = _brand_filename(filename)
+    with open(fname, 'rb') as f:
+        t = retrieve_table(f)
+        #print("Retrieved table:\n{}".format(t))
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from core import (
     process_frequencies,
     create_tree_code,
     parse_tree_code,
+    save_compressed_file,
+    retrieve_compressed_file,
 )
 
 
@@ -14,8 +16,11 @@ def compress(filename):
     with open(filename, 'r') as f:
         freqs = process_frequencies(f.read())
     t = create_tree_code(freqs)
-    d = parse_tree_code(t)
-    print(d)
+    table = parse_tree_code(t)
+    #print("Original:\n {}".format(table))
+    save_compressed_file(filename, table)
+    retrieve_compressed_file(filename)
+
 
 
 if __name__ == '__main__':
