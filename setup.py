d2df5cff66097dd5bc2405622339e935f9d94049
Mariano Anaya <marianoanaya@gmail.com>
2015-06-01 21:31:24 -0300

    Improve setup.py for #1.

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 from setuptools import setup
+from compressor.__version__ import VERSION
+
+
+with open('Readme.rst', 'r') as readme:
+    LONG_DESC = readme.read()
 
 
 setup(
     name='pycompress',
-    version='1.0',
+    version=VERSION,
     description='Py3 sample text compression application',
+    long_description=LONG_DESC,
     author='Mariano Anaya',
     author_email='marianoanaya@gmail.com',
+    url='https://github.com/rmariano/compr',
     packages=['compressor'],
+    zip_safe=True,
+    license='MIT',
+    keywords='text compression',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     entry_points={
         'console_scripts': [
             'pycompress = compressor:main',
