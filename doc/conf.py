b5cf85c8f9659620a125694b57ddec782cb33ed8
Mariano Anaya <marianoanaya@gmail.com>
2016-11-29 21:46:44 +0100

    Generate doc using Py3 annotations

--- doc/conf.py
+++ doc/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx_autodoc_annotation',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
