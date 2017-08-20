127ed4aa9dbd21063d2daa22be3cd14be20c3756
Mariano Anaya <marianoanaya@gmail.com>
2017-08-20 13:42:49 +0200

    cli-module: First split of the project
    Everything that was under ``compressor.__init__``, now is divided into:
    
    * compressor.cli: the command line commands
    * compressor.lib: the library API functionality
    
    Add ``compressor.__version__``, and update imports accordingly.
    
    Breaks backwards compatibility (imports will have to be updated!).

---
+++ compressor/lib.py
@@ -0,0 +1,24 @@
+"""compressor.lib
+
+High-level functions exposed as a library, that can be imported.
+"""
+from compressor.core import retrieve_compressed_file as extract_file  # noqa
+from compressor.core import (create_tree_code, parse_tree_code,
+                             process_frequencies, save_compressed_file)
+
+
+def compress_file(filename: str, dest_file: str = None) -> None:
+    """
+    Open the <filename> and compress its contents on a new one.
+
+    :param filename:  The path to the source file to compress.
+    :param dest_file: The name of the target file. If not provided (None),
+                      a default will be used with `<filename>.comp`
+    """
+    with open(filename, 'r') as source:
+        freqs = process_frequencies(source.read())
+
+    checksum = sum(c.freq for c in freqs)  # bytes
+    tree_code = create_tree_code(freqs)
+    table = parse_tree_code(tree_code)
+    save_compressed_file(filename, table, checksum, dest_file)
