ce9f30c541476463a0ab4dfeec2bf5c5098f8fb3
Mariano Anaya <marianoanaya@gmail.com>
2017-08-20 13:47:15 +0200

    cli-module: Use pytest!
    Refactored test to use pytest instead, and improve the tests code.

--- tests/functional/test_smoke.py
+++ tests/functional/test_smoke.py
@@ -20,24 +20,15 @@ def _all_files_identical(*files: List[str]) -> bool:
     return len(hashes) == 1
 
 
-class SmokeTest(BaseTest):
-    """Test that a reconstructed file (a file that was decompressed from
-    a previous compression process) is identical to the original.
+@pytest.mark.parametrize('source', data_files(data_store()))
+def test_compress_and_retrieve_datasets(source):
+    """Content must be unmodified, meaning the extracted file must
+    match the content prior compression.
     """
-
-    def test_compress_and_retrieve_datasets(self):
-        """Content must be unmodified, meaning the extracted file must
-        match the content prior compression.
-        """
-        for source in glob.glob(os.path.join(self.store, '*.txt')):
-            target = tempfile.NamedTemporaryFile().name
-            # Compress
-            main_engine(source, compress=True, dest_file=target)
-            # Now extract
-            extracted = tempfile.NamedTemporaryFile().name
-            main_engine(target, extract=True, compress=False, dest_file=extracted)
-            self.assertTrue(_all_files_identical(source, extracted), source)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    target = tempfile.NamedTemporaryFile().name
+    # Compress
+    main_engine(source, compress=True, dest_file=target)
+    # Now extract
+    extracted = tempfile.NamedTemporaryFile().name
+    main_engine(target, extract=True, compress=False, dest_file=extracted)
+    assert _all_files_identical(source, extracted)
--- tests/unit/test_processor.py
+++ tests/unit/test_processor.py
@@ -1,56 +1,52 @@
-import glob
+"""Unit tests the core functions"""
+import pytest
 import os
 import random
 
 from compressor.core import (create_tree_code, parse_tree_code,
                              process_frequencies)
-from tests import BaseTest
+from tests.conftest import data_files, data_store
 
 
-class TestCore(BaseTest):
-    """Unit tests in the core functions."""
+def test_stream_counter_data(data_store):
+    """The function that processes the frequencies correctly."""
+    with open(os.path.join(data_store, 'test001.txt'), 'r') as f:
+        result = process_frequencies(f.read())
 
-    def test_stream_counter_data(self):
-        """Check that the function that processes the frequencies, computes
-        correctly."""
-        with open(os.path.join(self.store, 'test001.txt'), 'r') as f:
-            result = process_frequencies(f.read())
-        expected = {'a': 45, 'b': 13, 'c': 11, 'd': 16, 'e': 9, 'f': 5, '\n': 1}
-        for node in result:
-            self.assertEqual(node.freq, expected[node.value])
+    mapped_result = {node.value: node.freq for node in result}
+    expected = {'a': 45, 'b': 13, 'c': 11, 'd': 16, 'e': 9, 'f': 5, '\n': 1}
 
-    def test_stream_counter_randomized(self):
-        """Generate a random string, and test the process_frequencies
-        function."""
-        selected_chars = ('a', 'Z', 'e', 'g', 'd', 's', 't', 'm', 'r', 'l')
-        stream = ''
-        expected = {k: 0 for k in selected_chars}
-        for _ in range(100):
-            multiplier = random.randint(1, 100)
-            char = random.choice(selected_chars)
-            stream += char * multiplier
-            expected[char] += multiplier
-        result = process_frequencies(stream)
-        for node in result:
-            self.assertEqual(node.freq, expected[node.value])
-
-    def test_preffix_free(self):
-        """Assert that the internal table generates new variable-length
-        prefix-free code."""
-        for source in glob.glob(os.path.join(self.store, '*.txt')):
-            with open(source, 'r') as f:
-                freqs = process_frequencies(f.read())
-            checksum = sum(c.freq for c in freqs)  # bytes
-            t = create_tree_code(freqs)
-            table = parse_tree_code(t)
-            codes = sorted(table.values(), key=lambda x: (len(x), x))
-            current_code = codes.pop(0)
-            while codes:
-                for code in codes:
-                    self.assertFalse(code.startswith(current_code),
-                                     "{} is prefix of {}".format(current_code, code))
-                current_code = codes.pop(0)
+    assert mapped_result == expected
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_stream_counter_randomized():
+    selected_chars = ('a', 'Z', 'e', 'g', 'd', 's', 't', 'm', 'r', 'l')
+    stream = ''
+    expected = {k: 0 for k in selected_chars}
+    for _ in range(100):
+        multiplier = random.randint(1, 100)
+        char = random.choice(selected_chars)
+        stream += char * multiplier
+        expected[char] += multiplier
+
+    result = process_frequencies(stream)
+    mapped_result = {node.value: node.freq for node in result}
+    assert mapped_result == expected
+
+
+@pytest.mark.parametrize('source', data_files(data_store()))
+def test_preffix_free(source):
+    """The internal table generates new variable-length prefix-free code."""
+    with open(source, 'r') as f:
+        freqs = process_frequencies(f.read())
+
+    checksum = sum(c.freq for c in freqs)  # bytes
+    t = create_tree_code(freqs)
+    table = parse_tree_code(t)
+    codes = sorted(table.values(), key=lambda x: (len(x), x))
+    current_code = codes.pop(0)
+    while codes:
+        for code in codes:
+            assert not code.startswith(current_code), \
+                    "{} is prefix of {}".format(current_code, code)
+        current_code = codes.pop(0)
