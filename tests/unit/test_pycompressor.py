e3ed91073f75070e6f1b8002a755bcb57f39912e
Mariano Anaya <marianoanaya@gmail.com>
2018-05-18 10:57:56 +0200

    Test extract/compress actions in PyCompressor

--- tests/unit/test_pycompressor.py
+++ tests/unit/test_pycompressor.py
@@ -1,5 +1,7 @@
+from unittest import mock
 import pytest
 
+from compressor.constants import Actions
 from compressor.pycompressor import PyCompressor
 
 
@@ -54,3 +56,23 @@ def test_pycompressor_destination_and_output_dir(
         output_dir=output,
     )
     assert app.destination == expected
+
+
+def test_pycompressor_extract_called():
+    extract_file = mock.Mock()
+    new_action = {Actions.EXTRACT: extract_file}
+    with mock.patch.object(PyCompressor, "ACTION_OPERATIONS", new=new_action):
+        PyCompressor(
+            "source", extract=True, compress=False, dest_file="output"
+        ).run()
+    extract_file.assert_called_with("source", "output")
+
+
+def test_pycompressor_compress_called():
+    compress_file = mock.Mock()
+    new_action = {Actions.COMPRESS: compress_file}
+    with mock.patch.object(PyCompressor, "ACTION_OPERATIONS", new=new_action):
+        PyCompressor(
+            "source", extract=False, compress=True, dest_file="output"
+        ).run()
+    compress_file.assert_called_with("source", "output")
